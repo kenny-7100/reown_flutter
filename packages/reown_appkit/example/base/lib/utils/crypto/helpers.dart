9e1685cc522d8e7f4c6742bb875ebc512689c243
kenny <9898983+wrule@users.noreply.github.com>
2025-11-01 20:21:33 +0800

    chore: rm unused code

--- packages/reown_appkit/example/base/lib/utils/crypto/helpers.dart
+++ packages/reown_appkit/example/base/lib/utils/crypto/helpers.dart
@@ -1,4 +1,3 @@
-import 'package:reown_appkit/reown_appkit.dart';
 import 'package:reown_appkit_dapp/utils/crypto/solana.dart';
 
 List<String> getChainMethods(String namespace) {
@@ -16,58 +15,5 @@ List<String> getChainEvents(String namespace) {
       return Solana.events;
     default:
       return [];
-  }
-}
-
-Future<SessionRequestParams?> getParams(
-  String method,
-  String address,
-  ReownAppKitModalNetworkInfo chainData, {
-  String? callback,
-}) async {
-  switch (method) {
-    case 'solana_signMessage':
-      final message = Solana.personalSignMessage();
-      return SessionRequestParams(
-        method: method,
-        params: {'pubkey': address, 'message': message},
-      );
-    case 'solana_signTransaction':
-    case 'solana_signAndSendTransaction':
-      final transactionV0_2 = await Solana.constructSolanaTX2(
-        address,
-        chainData,
-      );
-      final encodedV0Trx = Solana.serializeTransaction(transactionV0_2);
-
-      return SessionRequestParams(
-        method: method,
-        params: {
-          'transaction': encodedV0Trx,
-          'pubkey': address,
-          'feePayer': address,
-          ...transactionV0_2.message.toJson(),
-        },
-      );
-    case 'solana_signAllTransactions':
-      final transactionV0_1 = await Solana.constructSolanaTX(
-        address,
-        chainData,
-      );
-      final transactionV0_2 = await Solana.constructSolanaTX2(
-        address,
-        chainData,
-      );
-      final encodedV0Trx_1 = Solana.serializeTransaction(transactionV0_1);
-      final encodedV0Trx_2 = Solana.serializeTransaction(transactionV0_2);
-
-      return SessionRequestParams(
-        method: method,
-        params: {
-          'transactions': [encodedV0Trx_1, encodedV0Trx_2],
-        },
-      );
-    default:
-      return SessionRequestParams(method: method, params: null);
   }
 }
