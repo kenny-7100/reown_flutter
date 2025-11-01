189b4d3f7127a294c0e81c5b671627067f399662
kenny <9898983+wrule@users.noreply.github.com>
2025-11-01 20:36:52 +0800

    chore: rm unused code

--- packages/reown_appkit/example/base/lib/pages/connect_page.dart
+++ packages/reown_appkit/example/base/lib/pages/connect_page.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:reown_appkit/reown_appkit.dart';
 
-class ConnectPage extends StatefulWidget {
+class ConnectPage extends StatelessWidget {
   const ConnectPage({super.key, required this.appKitModal});
 
   final ReownAppKitModal appKitModal;
 
   @override
-  ConnectPageState createState() => ConnectPageState();
-}
-
-class ConnectPageState extends State<ConnectPage> {
-  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        AppKitModalConnectButton(appKit: widget.appKitModal),
+        AppKitModalConnectButton(appKit: appKitModal),
         Visibility(
-          visible: widget.appKitModal.isConnected,
-          child: AppKitModalAccountButton(appKitModal: widget.appKitModal),
+          visible: appKitModal.isConnected,
+          child: AppKitModalAccountButton(appKitModal: appKitModal),
         ),
       ],
     );
